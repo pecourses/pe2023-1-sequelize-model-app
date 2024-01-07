a18886d7500f5a58a055bd42ca873584782bbf3a
Viktoriia <pecourses>
2024-01-07 15:46:16 +0200

    Add user migration

---
+++ migrations/20240107132716-create-user.js
@@ -0,0 +1,53 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+// unique, allowNull, defaultValue
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      passw_hash: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      birthday: {
+        type: Sequelize.DATEONLY,
+      },
+      // male, female, other
+      gender: {
+        type: Sequelize.ENUM('male', 'female', 'other'),
+      },
+      image: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  },
+};
