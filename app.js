5bb23358e9ef49d7d087b07c992bff5a1a4260df
Viktoriia <pecourses>
2024-01-07 15:19:47 +0200

    Add sequelize

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ app.use(express.json());
 
 // Add endpoints handlers
 
+// POST /api/users {}
+// GET /api/users
+// GET /api/users/1
+// PATCH /api/users/1 {}
+// DELETE /api/users/1
+
 app.use(errorHandlers.errorHandler);
 
 module.exports = app;
