9e873c30e52a3aa46607456dba179e28f6a55034
Viktoriia <pecourses>
2024-01-13 20:02:38 +0200

    Add sequelize errors handlers

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use('/api', router);
 
-app.use(errorHandlers.errorHandler);
+app.use(errorHandlers.dbErrorHandler, errorHandlers.errorHandler);
 
 module.exports = app;
 
