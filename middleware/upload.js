7951bb33ff7b530df5b96e742d8a4c9ee79960b3
Viktoriia <pecourses>
2024-01-20 19:37:48 +0200

    Configured multer

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,6 +2,22 @@ const path = require('path');
 const multer = require('multer');
 const { STATIC_PATH } = require('./../constants');
 
-const upload = multer({ dest: path.join(STATIC_PATH, 'images') });
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(STATIC_PATH, 'images'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  },
+});
+
+function fileFilter (req, file, cb) {
+  // image/gif, image/jpef, image/png
+  const MIMETYPE_REG_EXP = /^image\/(gif|jpeg|png)$/;
+
+  cb(null, MIMETYPE_REG_EXP.test(file.mimetype)); // cb(null, чи_зберігати)
+}
+
+const upload = multer({ storage, fileFilter });
 
 module.exports.uploadUserPhoto = upload.single('userPhoto');
