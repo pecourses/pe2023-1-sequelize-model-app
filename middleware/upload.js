e25df6bd5b7eb86ee1c943c0b7261601bef27973
Viktoriia <pecourses>
2024-01-22 01:11:44 +0200

    Add multer error handler (extras)

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const multer = require('multer');
+const createHttpError = require('http-errors');
 const { STATIC_PATH } = require('./../constants');
 
 const storage = multer.diskStorage({
@@ -15,7 +16,14 @@ function fileFilter (req, file, cb) {
   // image/gif, image/jpef, image/png
   const MIMETYPE_REG_EXP = /^image\/(gif|jpeg|png)$/;
 
-  cb(null, MIMETYPE_REG_EXP.test(file.mimetype)); // cb(null, чи_зберігати)
+  // cb(null, MIMETYPE_REG_EXP.test(file.mimetype)); // cb(null, чи_зберігати)
+
+  // Якщо тип файлу допустимий, то зберігаємо
+  if (MIMETYPE_REGEXP.test(file.mimetype)) {
+    return cb(null, true);
+  }
+  // Інакше генеруємо помилку і перериваємо ланцюжок обробників
+  cb(createHttpError(415, 'Support only jpeg/png/gif mimetypes'));
 }
 
 const upload = multer({ storage, fileFilter });
